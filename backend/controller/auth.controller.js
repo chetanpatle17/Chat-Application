10bf0374e3554af68562b989e88cd8646ddc3317
chetanpatle17 <chetanpatle17p@gmail.com>
2024-08-18 16:26:09 +0530

    middleware to handle possible error

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/usermodel.js"
 import bcryptjs from "bcryptjs"
 import jwt from "jsonwebtoken"
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password, confirmPassword, gender} = req.body
 
     let validUser
@@ -10,16 +11,11 @@ export const signup = async (req, res) => {
     validUser = await User.findOne({email})
 
     if(validUser) {
-        return res.status(400).json({
-            success:false,
-            message: "User already exists"
-        })
+        return next(errorHandler(400, "User already exists"))
     }
 
     if(password != confirmPassword){
-        return res.status(400).json({
-            error: "Password don't match",
-        })
+        return next(errorHandler(400, "Password don't match"))
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10)
@@ -32,7 +28,7 @@ export const signup = async (req, res) => {
         email,
         password: hashedPassword,
         gender,
-        profilePic: gender === "male" ? boyProfilePic:girlProfilePic,
+        profilePic: gender === "male" ? boyProfilePic:girlProfilePic
     })
     try {
         //generate jwt token
@@ -48,10 +44,7 @@ export const signup = async (req, res) => {
         })
 
     } catch (error) {
-        console.log("Error"+error);
-        res.status(500).json({
-            error: "Internal Server Error",
-        })
+        next(error)
     }
 }
 
