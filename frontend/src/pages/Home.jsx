c0fa6ca838ca6d5cc58dfa25af0f2a0b40ec3b09
chetanpatle17 <chetanpatle17p@gmail.com>
2024-08-23 23:59:21 +0530

    Homepage Sidebar UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Sidebar from '../component/sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200">
+      <Sidebar/>
+    </div>
   )
 }
 
