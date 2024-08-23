bf9313a25d44f9fa3aea68f524b4ca14ed79b11a
chetanpatle17 <chetanpatle17p@gmail.com>
2024-08-23 22:51:52 +0530

    Signup page UI

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import GenderCheckbox from '../component/GenderCheckbox'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className = "flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-200">
+      <h1 className="text-3x1 font-semibold text-center text-slate-700">Signup to 
+            <span className='text-blue-700'> Chat Application</span>
+            </h1>
+            <form>
+            <div>
+                    <label className="label p-2">
+                        <span
+                        className="text-base label-text">Username</span>
+                    </label>
+                    <input type="text" 
+                    placeholder="Enter Your Username"
+                    className="w-full input input bordered h-10"/>
+                </div>
+                <div>
+                    <label className="label p-2">
+                        <span
+                        className="text-base label-text">Email</span>
+                    </label>
+                    <input type="email" 
+                    placeholder="Enter Your Email"
+                    className="w-full input input bordered h-10"/>
+                </div>
+                <div>
+                    <label className="label p-2">
+                        <span
+                        className="text-base label-text">Password</span>
+                    </label>
+                    <input type="text" 
+                    placeholder="Enter Password"
+                    className="w-full input input bordered h-10"/>
+                </div>
+                <GenderCheckbox/>
+
+                <Link to={"/login"} className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">Already have an account?</Link>
+                <div>
+                    <button className="btn btn-block btn-sm mt-2 bg-blue-700 font-bold text-white">Sign up</button>
+                </div>
+            </form>
+      </div>
+      </div>
   )
 }
 
