8b016f09fb8cd5378e313b07942c32e019b081f1
chetanpatle17 <chetanpatle17p@gmail.com>
2024-08-24 14:53:58 +0530

    Login functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import useLogin from '../hooks/useLogin'
 const Login = () => {
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    const {login,loading} = useLogin()
+    const handleSubmit = async(e)=>{
+        e.preventDefault()
+        await login(email,password)
+    }
+
+
   return (
     <div className = "flex flex-col items-center justify-center min-w-96 mx-auto">
         <div className="w-full p-6 rounded-lg shadow-md bg-gray-200">
             <h1 className="text-3x1 font-semibold text-center text-slate-700">Login to 
             <span className='text-blue-700'> Chat Application</span>
             </h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label className="label p-2">
                         <span
                         className="text-base label-text">Email</span>
                     </label>
-                    <input type="text" 
-                    placeholder="Enter Your Email"
-                    className="w-full input input bordered h-10"/>
+                    <input type="email" 
+                    placeholder="Enter Email"
+                    className="w-full input input bordered h-10"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                    />
                 </div>
 
                 <div>
@@ -25,11 +38,17 @@ const Login = () => {
                     </label>
                     <input type="password" 
                     placeholder="Enter Password"
-                    className="w-full input input bordered h-10"/>
+                    className="w-full input input bordered h-10"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <Link to={"/signup"} className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">{"Don't"} have an account?</Link>
                 <div>
-                    <button className="btn btn-block btn-sm mt-2 bg-blue-700 font-bold text-white">Login</button>
+                    <button className="btn btn-block btn-sm mt-2 bg-blue-700 font-bold text-white" disabled={loading}>
+                        {loading?(
+                            <span className="loading loading-spinner"></span>
+                        ):("Login")}
+                    </button>
                 </div>
             </form>
         </div>
