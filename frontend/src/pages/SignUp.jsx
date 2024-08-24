8bf0a01561eee0b2faaa380ebdec8d8f4682fb10
chetanpatle17 <chetanpatle17p@gmail.com>
2024-08-24 11:44:48 +0530

    Sign Up functionality

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GenderCheckbox from '../component/GenderCheckbox'
 import { Link } from 'react-router-dom'
+import useSignup from '../hooks/useSignup'
 
 const SignUp = () => {
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        gender: "",
+    })
+
+    const { loading, signup} = useSignup()
+
+    const handleCheckboxChange = (gender) => {
+        setFormData({...formData, gender})
+    }
+    const handleSubmit=async(e) => {
+        e.preventDefault()
+        await signup(formData)
+    }
+
   return (
     <div className = "flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-200">
       <h1 className="text-3x1 font-semibold text-center text-slate-700">Signup to 
             <span className='text-blue-700'> Chat Application</span>
             </h1>
-            <form>
+            <form onSubmit={handleSubmit}>
             <div>
                     <label className="label p-2">
                         <span
                         className="text-base label-text">Username</span>
                     </label>
                     <input type="text" 
-                    placeholder="Enter Your Username"
-                    className="w-full input input bordered h-10"/>
+                    placeholder="Enter Username"
+                    className="w-full input input bordered h-10"
+                    value = {formData.username}
+                    onChange = {(e)=>
+                        setFormData({...formData, username: e.target.value})
+                    }
+                    />
                 </div>
                 <div>
                     <label className="label p-2">
@@ -26,18 +50,47 @@ const SignUp = () => {
                     </label>
                     <input type="email" 
                     placeholder="Enter Your Email"
-                    className="w-full input input bordered h-10"/>
+                    className="w-full input input bordered h-10"
+                    value = {formData.email}
+                    onChange = {(e)=>
+                        setFormData({...formData, email: e.target.value})
+                    }
+                    />
                 </div>
                 <div>
                     <label className="label p-2">
                         <span
                         className="text-base label-text">Password</span>
                     </label>
-                    <input type="text" 
+                    <input type="password" 
                     placeholder="Enter Password"
-                    className="w-full input input bordered h-10"/>
+                    className="w-full input input bordered h-10"
+                    value = {formData.password}
+                    onChange = {(e)=>
+                        setFormData({...formData, password: e.target.value})
+                    }
+                    />
                 </div>
-                <GenderCheckbox/>
+
+                <div>
+                    <label className="label p-2">
+                        <span
+                        className="text-base label-text">Confirm Password</span>
+                    </label>
+                    <input type="password" 
+                    placeholder="Confirm Password"
+                    className="w-full input input bordered h-10"
+                    value = {formData.confirmPassword}
+                    onChange = {(e)=>
+                        setFormData({...formData, confirmPassword: e.target.value})
+                    }
+                    />
+                </div>
+
+                <GenderCheckbox 
+                onCheckboxChange={handleCheckboxChange}
+                selectedGender={formData.gender}
+                />
 
                 <Link to={"/login"} className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block">Already have an account?</Link>
                 <div>
