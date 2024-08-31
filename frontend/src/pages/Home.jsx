a7dbca621fef6612daa5e832002781f01d5efc11
chetanpatle17 <chetanpatle17p@gmail.com>
2024-08-31 18:46:23 +0530

    background-change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import MessageContainer from '../component/messages/MessageContainer'
 
 const Home = () => {
   return (
-    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200">
+    <div className="flex sm:h-[750px] md:h-[650px] rounded-lg overflow-hidden bg-gray-400">
       <Sidebar/>
       <MessageContainer />
     </div>
