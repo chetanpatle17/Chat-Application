8bb4837e24f519278a51ccb118900a4f9808cb2a
chetanpatle17 <chetanpatle17p@gmail.com>
2024-09-04 18:55:59 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://chat-application-lhqh.onrender.com",
         secure: false,
       },
     },
